c70d78b89679a49ffb3124e4d8cd2111f10b2b53
felipealbarregas <55880228+felipealbarregas@users.noreply.github.com>
2020-05-25 21:50:16 +0200

    agregar funcion completar categoria

---
+++ php/obtenercategoria.php
@@ -0,0 +1,19 @@
+<?php
+
+
+include('../includes/dbconnection.php' );
+
+
+      $consulta = "SELECT ID, Nombre FROM categoria ";
+      $resultado = $conexion->query($consulta)or die($conexion->error);
+  while($data = mysqli_fetch_array($resultado)){
+      $subcategoria[] =$array=array(
+          'categoria_id'=>$data['ID'],
+          'categoria_nombre'=>$data['Nombre'],
+      );
+  }
+
+    echo json_encode($subcategoria);
+
+      mysqli_close($conexion);
+?>
